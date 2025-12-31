7d6b1adf8d173a18e37baddf120041dc9bf863aa
Andreas Hoch <andreas_hoch@gmx.de>
2025-12-31 11:26:59 +0100

    Add authentication and proxy setup

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+
+// This is only used by Proxy and gets extended later on in auth.ts
+// protected routes will not even start rendering until the Proxy verifies the authentication, enhancing both the security and performance of your application.
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
